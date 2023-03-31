a38d4f588bbda1f0abe34fc10ccd1335b5e005f2
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-03-31 10:06:10 +0200

    feat: add new metrics

--- Network-analysis/utilities.py
+++ Network-analysis/utilities.py
@@ -5,8 +5,11 @@ import sys
 sys.path.append('../')
 print(sys.path)
 
-def create_graph_from_df(df, edge_attr=None, source='SourceID', target='TargetID'):
-    return nx.from_pandas_edgelist(df, source=source, target=target, edge_attr=edge_attr)
+def create_graph_from_df(df, edge_attr=None, source='SourceID', target='TargetID', isDirected=False):
+    create_using = None
+    if isDirected: 
+        create_using = nx.DiGraph() 
+    return nx.from_pandas_edgelist(df, source=source, target=target, edge_attr=edge_attr, create_using=create_using)
 
 def create_df_from_edge_list(edgeListPath, names=['SourceID','TargetID','Day']):
     return pd.read_csv(edgeListPath, sep='\s+', header=None, names=names)
